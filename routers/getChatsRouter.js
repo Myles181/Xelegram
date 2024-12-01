afd7aca918a986ecc254b1e4f65ab4a97a58b476
Myles181 <cyrile450@gmail.com>
2024-12-01 13:13:48 -0800

    Chats

--- routers/getChatsRouter.js
+++ routers/getChatsRouter.js
@@ -53,8 +53,8 @@ const router = express.Router();
  *                         example: "User"
  *                       isMember:
  *                         type: string
- *                          description: Identify if a user is a member of a channel. If type is a User then member will by default be true
- *                          example: true
+ *                         description: Identify if a user is a member of a channel. If type is a User then member will by default be true
+ *                         example: true
  *                       unreadMessagesCount:
  *                         type: integer
  *                         description: Number of unread messages in the chat (default is 0).
