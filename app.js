92e98ed485ecf19f84ac4d527f41d5ac8ace29d9
Myles181 <cyrile450@gmail.com>
2024-12-01 03:13:25 -0800

    App

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const ReqError = require("./utilities/ReqError");
 const errorController = require("./controllers/errorController");
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
