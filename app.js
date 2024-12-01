2b0f92f1164f4c1f7ccf22feb90d04a88893e03b
Myles181 <cyrile450@gmail.com>
2024-12-01 00:38:01 -0800

    Removed the client directory, Removed database integration and added getChats api

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
-// Telegram
+
+// Telegram Routers
 const authTelegramRouter = require("./routers/authRouter");
 const joinChannelTelegramRouter = require("./routers/joinChannelRouter");
 const messageUserTelegramRouter = require("./routers/messageUserRouter");
 const searchChannelTelegramRouter = require("./routers/searchChannelRouter");
 const searchUserTelegramRouter = require("./routers/searchUserRouter");
-const getChatsRouter = require("./routers/getChatsRouter")
-
+const getChatsRouter = require("./routers/getChatsRouter");
 
 const ReqError = require("./utilities/ReqError");
 const errorController = require("./controllers/errorController");
@@ -26,18 +26,27 @@ app.use(
     cookie: {
       secure: false, // Set to true if HTTPS is used
       httpOnly: true, // Prevents JavaScript from accessing the cookie
-      sameSite: "lax" // Controls cookie sharing across sites
-    }
+      sameSite: "lax", // Controls cookie sharing across sites
+    },
   })
 );
 
-// Use session before defining any routes
+// CORS Configuration
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://xelegram.onrender.com", "http://localhost:5501"], // Allowed origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed request headers
+  credentials: true, // Allow cookies to be sent
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
 
-
-// Telegram
+// Telegram Routes
 app.use("/t/api", authTelegramRouter);
 app.use("/t/api", joinChannelTelegramRouter);
 app.use("/t/api", messageUserTelegramRouter);
@@ -45,8 +54,7 @@ app.use("/t/api", searchChannelTelegramRouter);
 app.use("/t/api", searchUserTelegramRouter);
 app.use("/t/api", getChatsRouter);
 
-// Error handle middleware
+// Error Handling Middleware
 app.use(errorController);
-
 
 module.exports = app;
